57af46bf70904117bb3f9483e45739b129dcd752
Alexander Soen <alexandersoen@gmail.com>
2026-02-22 20:00:47 +0100

    Add import order sorting for python formatting

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,21 +1,22 @@
 local conform = require("conform")
 
 conform.setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    tex = { "tex-fmt" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		tex = { "tex-fmt" },
+		python = { "ruff_organize_imports", "ruff_format" },
+	},
 })
 
 conform.formatters["tex-fmt"] = {
-  prepend_args = { "--nowrap" },
+	prepend_args = { "--nowrap" },
 }
 
 vim.keymap.set("n", "gq", function()
-  print("Formatting")
-  conform.format({
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500
-  })
+	print("Formatting")
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
 end, { desc = "Format" })
